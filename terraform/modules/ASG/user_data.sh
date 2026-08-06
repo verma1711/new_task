c9d3a73f12218384d8f65d24efc08f768592e439
anurag <boss14220@gmail.com>
2026-08-06 19:43:36 +0530

    changed instance type

--- terraform/modules/ASG/user_data.sh
+++ terraform/modules/ASG/user_data.sh
@@ -8,9 +8,25 @@ dnf install -y docker ruby wget --allowerasing
 systemctl enable docker
 systemctl start docker
 
+mkdir -p /usr/local/lib/docker/cli-plugins
+
+wget \
+  https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+  -O /usr/local/lib/docker/cli-plugins/docker-compose
+
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+
 cd /tmp
 
-wget -O install https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+wget -O install \
+  https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 
 chmod +x install
 ./install auto
